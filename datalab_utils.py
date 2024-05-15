e75bf9ea9b32930058024adf27f69536b422902b
adriansh95 <ashestak@ucsc.edu>
2024-05-15 19:12:42 +0000

    changed argrelextrema to find_peaks and reduced sigma_threshold (will revert to 5)

--- datalab_utils.py
+++ datalab_utils.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.stats import gaussian_kde
-from scipy.signal import argrelextrema
-from sklearn.neighbors import KernelDensity
+from scipy.signal import find_peaks
 
 def weighted_std(vals, weights):
     result = np.sqrt(np.cov(vals, aweights=weights).item())
@@ -21,8 +20,8 @@ def kde_pdf(samples, weights, bandwidth):
 
 def label_cluster_membership(samples, kde_result):
     pdf, x = kde_result["pdf"], kde_result["x"]
-    maxima = argrelextrema(pdf, np.greater)[0]
-    minima = argrelextrema(pdf, np.less)[0]
+    maxima = find_peaks(pdf)[0]
+    minima = find_peaks(-pdf)[0]
     n_maxima = len(maxima)
 
     if n_maxima == 1:
@@ -32,14 +31,14 @@ def label_cluster_membership(samples, kde_result):
 
     return result
 
-def _label_cluster_type(samples, weights, kde_result):
+def _label_cluster_type(samples, weights, kde_result, tolerance):
     result = -1
     pdf, x = kde_result["pdf"], kde_result["x"]
-    a, b = 1.7, 2.3
+    a, b = tolerance
     high_cutoff, low_cutoff = -2.5 * np.log10(a), -2.5 * np.log10(b)
-    sigma_cutoff = 2.5 * np.log10(a) / 5
-    maxima = argrelextrema(pdf, np.greater)[0]
-    minima = argrelextrema(pdf, np.less)[0]
+    sigma_cutoff = 2.5 * np.log10(1.7) / 5
+    maxima = find_peaks(pdf)[0]
+    minima = find_peaks(-pdf)[0]
     n_maxima = len(maxima)
 
     if n_maxima == 1:
@@ -68,62 +67,39 @@ def _label_cluster_type(samples, weights, kde_result):
             tol2 = (samples_bright < weighted_mu + high_cutoff)
             condition1 = all(tol1 & tol2)
             condition2 = (weighted_sigma <= sigma_cutoff)
-            condition3 = all(delta_significance >= 5)
+#             condition3 = all(delta_significance >= 5)
+            condition3 = all(delta_significance >= 3) #DELETE ME
 
             if condition1 and condition2 and condition3:
                 result = 2
 
     return result
 
-def _kde_cluster_label(df, mag_column, magerr_column, bandwidth):
-    samples = df[mag_column].values
-    weights = np.power(df[magerr_column].values, -2)
-    kde_result = kde_pdf(samples, weights, bandwidth)
-    cluster_type = _label_cluster_type(samples, weights, kde_result)
-
-    if cluster_type == -1:
-        cluster_membership = np.full(len(samples), -1)
-    else:
-        cluster_membership = label_cluster_membership(samples, kde_result)
-
-    d_keys = list(df.columns) + ["cluster_label"]
-    d_vals = [df[col].values for col in df.columns] + [cluster_membership]
-    d = {k: v for k, v in zip(d_keys, d_vals)}
-    result = pd.DataFrame(d)
+def cluster_label_dataframe(df, 
+                            mag_column="mag_auto", 
+                            magerr_column="magerr_auto", 
+                            bandwidth=0.1254,
+                            tolerance=(1.7, 2.3)):
+    g = df[["objectid", "filter", mag_column, magerr_column]].groupby(by=["objectid", "filter"])
+    cluster_label = g.apply(_cl_apply, bandwidth, tolerance)
+    result = df.assign(cluster_label=cluster_label)
     return result
 
-def _cl_transform(s, bandwidth):
-    samples = s.transform(lambda x: x[0]).values
-    weights = np.power(s.transform(lambda x: x[1]).values, -2)
+def _cl_apply(df, bandwidth, tolerance):
+    samples = df[df.columns[2]].values
+    weights = np.power(df[df.columns[3]].values, -2)
+    idxs = df.index
     kde_result = kde_pdf(samples, weights, bandwidth)
-    cluster_type = _label_cluster_type(samples, weights, kde_result)
+    cluster_type = _label_cluster_type(samples, weights, kde_result, tolerance)
 
     if cluster_type == -1:
         result = np.full(len(samples), -1)
     else:
         result = label_cluster_membership(samples, kde_result)
 
-    return result
-    
+    return pd.DataFrame(data=result, index=idxs)
 
-def cluster_label_dataframe(df, mag_column="mag_auto", 
-                            magerr_column="magerr_auto", bandwidth=0.1254):
-    mx = pd.MultiIndex.from_frame(df[["objectid", "filter"]])
-    vals = list(df[[mag_column, magerr_column]].values)
-    s = pd.Series(vals, index=mx)
-    g = s.groupby(by=["objectid", "filter"])
-    cluster_labelled = g.transform(_cl_transform, bandwidth)
-    d_keys = list(df.columns) + ["cluster_label"]
-    d_vals = [df[col].values for col in df.columns] + [cluster_labelled.values]
-    d = {k: v for k, v in zip(d_keys, d_vals)}
-    result = pd.DataFrame(d)
-    return result
-
-def filter_cluster_labelled_df(df, time_column="mjd"):
-    result = df.groupby(by=["objectid"]).filter(_filter_cl_df)
-    return result
-    
-def _filter_cl_df(df):
+def lens_filter(df):
     result = False
     condition1 = ~((df["cluster_label"] == -1).any())
     condition2 = (df["cluster_label"] == 0).any()
@@ -146,53 +122,49 @@ def _filter_cl_df(df):
     return result
 
 def make_lensing_dataframe(df, time_column="mjd", exp_time_column="exptime"):
-    """This function assumes the dataframe has been sorted by the time_column"""
-    df_grouped = df.groupby(by=["objectid"])
-    s = df_grouped.apply(_lens_apply, time_column, exp_time_column)
-    s.name = "lens_data"
-    result = s.reset_index()
-    result['t_start'] = result["lens_data"].transform(lambda x: x.get('t_start'))
-    result['t_end'] = result["lens_data"].transform(lambda x: x.get('t_end'))
-    result['filters'] = result["lens_data"].transform(lambda x: x.get('filters'))
-    result.drop(columns="lens_data", inplace=True, axis=1)
-    result.dropna(inplace=True)
+    """This function assumes the dataframe has been sorted by the time_column
+    and filtered using lens_filter"""
+    column_list = ["objectid", time_column, exp_time_column, "cluster_label", "filter"]
+    df_grouped = df[column_list].groupby(by=["objectid"])
+    result = df_grouped.apply(_lens_apply)
+    result.reset_index(level=1, inplace=True, drop=True)
     return result
 
-def _lens_apply(df, time_column, exp_time_column):
-    result = {"t_start": np.nan, "t_end": np.nan, "filters": np.nan}
+def _lens_apply(df):
     s_per_day = 86400
-    condition1 = ~((df["cluster_label"] == -1).any())
-    condition2 = (df["cluster_label"] == 0).any()
+    mask_bright = df["cluster_label"] == 0
+    starts_lensed = mask_bright.iloc[0]
+    ends_lensed = mask_bright.iloc[-1]
+    n_lensed = sum(mask_bright)
+    n_total = len(df)
 
-    if condition1 and condition2:
-        mask_baseline = df["cluster_label"] == 1    
-        n_total = len(df)
-        n_bright = len(df[~mask_baseline])
-        idxs = np.arange(n_total)
-        baseline_idxs = idxs[mask_baseline]
-        idx_diffs = np.diff(baseline_idxs)
-        boundary_idxs = np.where(idx_diffs == (n_bright + 1))[0]
-        case1 = len(boundary_idxs) == 1
-        case2 = baseline_idxs[0] == n_bright
-        case3 = (n_total - 1) - baseline_idxs[-1] == n_bright
+    if starts_lensed:
+        t_end_idx = n_lensed
+        t_start = np.nan
+        t_end = df.iat[t_end_idx, 1]
+    elif ends_lensed:
+        t_start_idx = -(n_lensed + 1)
+        t_start = df.iat[t_start_idx, 1] + (df.iat[t_start_idx, 2] / s_per_day)
+        t_end = np.nan
+    else:
+        idx_range = np.arange(n_total)
+        t_start_idx = idx_range[mask_bright].min() - 1
+        t_end_idx = idx_range[mask_bright].max() + 1
+        t_start = df.iat[t_start_idx, 1] + (df.iat[t_start_idx, 2] / s_per_day)
+        t_end = df.iat[t_end_idx, 1]
+    filters = ''.join(df.loc[mask_bright, "filter"])
+    result = pd.DataFrame(data={"t_start": [t_start], "t_end": [t_end], "filters": [filters]})
+    return result
 
-        if case1:
-            idx0, idx1 = baseline_idxs[boundary_idxs[0]], baseline_idxs[boundary_idxs[0] + 1]
-            t_start = df.iloc[idx0][time_column] + (df.iloc[idx0][exp_time_column] / s_per_day)
-            t_end = df.iloc[idx1][time_column]
-        elif case2:
-            idx0, idx1 = -1, baseline_idxs[0]
-            t_start = np.nan
-            t_end = df.iloc[idx1][time_column]
-        elif case3:
-            idx0, idx1 = baseline_idxs[-1], n_total
-            t_start = df.iloc[idx0][time_column] + (df.iloc[idx0][exp_time_column] / s_per_day)
-            t_end = np.nan
+def subtract_baseline(df, mag_column="mag_auto", magerr_column="magerr_auto"):
+    df_grouped = df.groupby(by=["objectid", "filter"], group_keys=False)
+    result = df_grouped.apply(_subtract_baseline_apply, mag_column, magerr_column)
+    return result
 
-        if case1 or case2 or case3:
-            lensed_idxs = np.arange(idx0 + 1, idx1)
-            filters = ''.join(sorted(df.iloc[lensed_idxs]["filter"]))
-            exposures = df.iloc[lensed_idxs]["exposure"].values
-            result = {"t_start": t_start, "t_end": t_end, "filters": filters, "exposures": exposures}
-
+def _subtract_baseline_apply(df, mag_column, magerr_column):
+    mask_baseline = df["cluster_label"] == 1
+    samples_baseline = df.loc[mask_baseline, mag_column].values
+    weights_baseline = df.loc[mask_baseline, magerr_column].values**-2
+    baseline = np.average(samples_baseline, weights=weights_baseline)
+    result = df.assign(delta_mag=df[mag_column] - baseline)
     return result
