029874f5d80bad632878d20b0191a97d21889515
Adrian Shestakov <adrianshestakov@gmail.com>
2024-09-25 17:12:58 +0000

    finished LcScanner and moved filter_map to helpers.py

--- utils/effective_monitoring_time.py
+++ utils/effective_monitoring_time.py
@@ -15,27 +15,48 @@ class _FilterState():
         self.n_bright = n_bright
         self.n_baseline = self.n_all - n_bright
 
+    def reset(self):
+        """Reset self to initial values"""
+        n_taus = self.n_bright.shape[0]
+        self.n_all = np.zeros(7, dtype=np.int32)
+        self.n_bright = np.zeros((n_taus, 7), dtype=np.int32)
+        self.n_baseline = np.zeros((n_taus, 7), dtype=np.int32)
+
 class _TimeState():
     def __init__(self, n_taus):
         self.start_idx = np.zeros(n_taus, dtype=np.int32)
-        self.end_idx = np.ones(n_taus, dtype=np.int32)
+        self.end_idx = np.zeros(n_taus, dtype=np.int32)
+        self.t_start = np.zeros(n_taus)
 
-    def update(self, start_idx, end_idx):
+    def update(self, start_idx, end_idx, t_start):
         """update self"""
         self.start_idx = start_idx
         self.end_idx = end_idx
+        self.t_start = t_start
+
+    def reset(self):
+        """Reset self to initial values"""
+        n_taus = self.t_start.shape[0]
+        self.start_idx = np.zeros(n_taus, dtype=np.int32)
+        self.end_idx = np.zeros(n_taus, dtype=np.int32)
+        self.t_start = np.zeros(n_taus)
 
 class _LcScannerState():
     def __init__(self, n_taus):
         self.time = _TimeState(n_taus)
         self.filter = _FilterState(n_taus)
-        self.good_window = np.full(n_taus, False)
+        self.last_window_good = np.full(n_taus, False)
+        self.this_window_good = np.full(n_taus, False)
+
+    def reset(self):
+        """Reset the scanner's state"""
+        self.time.reset()
+        self.filter.reset()
+        self.last_window_good = np.full(self.this_window_good.shape, False)
+        self.this_window_good = np.full(self.this_window_good.shape, False)
 
 class LcScanner():
     """This class includes the record_windows method and its helpers"""
-    FILTERS = np.array(['u', 'g', 'r', 'i', 'z', 'Y', "VR"])
-    FILTER_ORDER = {'u': 0, 'g': 1, 'r': 2, 'i': 3, 'z': 4, 'Y': 5, 'VR': 6}
-
     def __init__(self, taus, n_filters_req=2):
         n_taus = len(taus)
         self.n_taus = n_taus
@@ -44,109 +65,77 @@ class LcScanner():
         self.n_filters_req = n_filters_req
         self.state = _LcScannerState(n_taus)
 
-    def update(self, start_idx, end_idx, n_bright):
+    def update(self, start_idx, end_idx, t_start, n_bright):
         """Update the scanner's state"""
-        self.state.time.update(start_idx, end_idx)
+        self.state.time.update(start_idx, end_idx, t_start)
         self.state.filter.update(n_bright)
-        self.state.good_window = self._is_good_window()
-
-    @classmethod
-    def _filter_map(cls, char):
-        result = cls.FILTER_ORDER[char]
-        return result
-
-    @classmethod
-    def filter_map(cls, char):
-        """This method maps [u, g, r, i, z, Y] to [0, 1, 2, 3, 4, 5]"""
-        mapper = np.vectorize(cls._filter_map)
-        result = mapper(char)
-        return result
+        self.state.last_window_good = self.state.this_window_good
+        self.state.this_window_good = self._is_good_window()
 
     def record_windows(self, dataframe):
         """This method is used to scan a lightcurve and find time windows
         during which a lensing event of duration tau could begin and
         would, in principle, be detectable"""
-        filters = dataframe["filter"].values
+        filter_idx = dataframe["filter_index"].values
         sample_times = (dataframe["mjd"] +
                         (dataframe["exptime"] / (86400 * 2))).values
-        result = self._record_windows(sample_times, filters)
+        result = self._record_windows(sample_times, filter_idx)
         return result
 
-    def _record_windows(self, times, filters):
-        self._setup(times, filters)
+    def _record_windows(self, times, filter_idx):
+        self._setup(times, filter_idx)
         result = np.full((self.n_samples * 2, self.n_taus), np.nan)
-        result[0] = np.where(self.state.good_window,
-                             times[self.state.time.end_idx - 1] - self.taus,
-                             np.nan)
-        result[1] = np.where(self.state.good_window,
-                             times[self.state.time.start_idx] + self.taus,
-                             np.nan)
 
         #shift, record, repeat
-        for i in range(self.n_samples - 1): # How many iterations?
-            self._shift_window(times, filters)
-            result[i] = np.where(self.state.good_window,
-                                 self._earliest_start(times),
+        for i in range(self.n_samples * 2):
+            self._shift_window(times, filter_idx)
+            window_changed = (self.state.last_window_good !=
+                              self.state.this_window_good)
+            result[i] = np.where(window_changed,
+                                 self.state.time.t_start,
                                  np.nan)
-            result[i+1] = np.where(self.state.good_window,
-                                   times[self.state.time.start_idx] + self.taus,
-                                   np.nan)
 
+        result = result[~np.isnan(result).all(axis=1)]
+        result.sort(axis=0)
         return result
 
-    def _earliest_start(self, times):
-        result = np.maximum(times[self.state.time.start_idx - 1],
-                            times[self.state.time.end_idx - 1] - self.taus)
-        return result
+    def _shift_window(self, times, filter_idx):
+        dt_start = times[self.state.time.start_idx] - self.state.time.t_start
+        end_idx_temp = np.clip(self.state.time.end_idx,
+                               a_min=None,
+                               a_max=self.n_samples - 1)
+        dt_end_temp = (times[end_idx_temp] -
+                       (self.state.time.t_start + self.taus))
+        dt_end = np.where(self.state.time.end_idx < self.n_samples,
+                          dt_end_temp,
+                          np.inf)
+        smaller_dt_start = dt_start < dt_end
+        new_start_idx = np.where(smaller_dt_start,
+                                 self.state.time.start_idx + 1,
+                                 self.state.time.start_idx)
+        new_end_idx = np.where(~smaller_dt_start,
+                               self.state.time.end_idx + 1,
+                               self.state.time.end_idx)
+        dt = np.where(smaller_dt_start, dt_start, dt_end)
+        new_t_start = self.state.time.t_start + dt
+        new_n_bright = self._compute_n_bright(filter_idx, new_start_idx, new_end_idx)
+        self.update(new_start_idx, new_end_idx, new_t_start, new_n_bright)
 
-    def _shift_window(self, times, filters):
-        new_start_idx = self.state.time.start_idx + 1
-        new_end_idx = self._compute_end_idx(times)
-        new_n_bright = self._compute_n_bright(filters, new_start_idx, new_end_idx)
-        self.update(new_start_idx, new_end_idx, new_n_bright)
-
-    def _compute_end_idx(self, times):
-        old_idx = self.state.time.end_idx
-        t_end = times[self.state.time.start_idx] + self.taus
-        result = self._numba_end_idx(times,
-                                     old_idx,
-                                     t_end,
-                                     self.n_samples,
-                                     self.n_taus)
-        return result
-
-    @staticmethod
-    @njit
-    def _numba_end_idx(times, old_idx, t_end, n_samples, n_taus):
-        result = [np.argmax(times[old_idx[i]:] > t_end[i]) + old_idx[i]
-                  for i in range(n_taus)]
-        result = np.array(result)
-        result[result == 0] = n_samples
-        return result
-
-    def _setup(self, times, filters):
+    def _setup(self, times, filter_idx):
+        self.state.reset()
         n_samples = len(times)
         self.n_samples = n_samples
-
-        # Setup Time stuff
-        start_idx = self.state.time.start_idx
-        end_idx = self._compute_end_idx(times)
-
-        # Filter stuff
         np.add.at(self.state.filter.n_all,
-                  self.filter_map(filters),
+                  filter_idx,
                   1)
-        n_bright = self._compute_n_bright(filters, start_idx, end_idx)
-        self.update(start_idx,
-                    end_idx,
-                    n_bright)
 
-    def _compute_n_bright(self, filters, start_idx, end_idx):
+    def _compute_n_bright(self, filter_idx, start_idx, end_idx):
         result = np.zeros(self.state.filter.n_bright.shape)
 
         for i in range(self.n_taus):
+            sti = start_idx[i]
             endi = end_idx[i]
-            f_idx = self.filter_map(filters[start_idx:endi])
+            f_idx = filter_idx[sti:endi]
             np.add.at(result[i], f_idx, 1)
 
         return result
