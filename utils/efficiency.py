f124a8eb172073e6e56b482a1eda207797d445c9
Adrian Shestakov <adrianshestakov@gmail.com>
2024-10-19 00:42:57 +0000

    migrated joins and random select from pandas to SQL

--- utils/efficiency.py
+++ utils/efficiency.py
@@ -1,62 +1,96 @@
 import numpy as np
 import pandas as pd
+import os
+import time
 
 from dl import queryClient as qc
 from dl.helpers.utils import convert
 from utils.kde_label import cluster_label_dataframe
-from utils.filtering import lens_filter
+from utils.helpers import get_bounding_idxs
+from pyarrow.lib import ArrowInvalid
 
-def make_lens_df(unimodal_ids, tau_window):
-    mask = tau_window.index.isin(unimodal_ids["objectid"], level=0)
-    # print(unimodal_ids, tau_window.index.get_level_values(0), mask.any())
-    # mask = np.full(len(tau_window), True) #DELETE
-    rng = np.random.default_rng()
-    masked_windows_df = tau_window.loc[mask]
-    weight_df = (masked_windows_df.xs("end", level=2)
-                 - masked_windows_df.xs("start", level=2))
-    oids = weight_df.index.get_level_values(0)
-    win_nums = weight_df.index.get_level_values(1)
-    weights = weight_df.values / weight_df.sum()
-    all_choices = list(zip(oids, win_nums))
-    choices = rng.choice(all_choices, size=1000, p=weights)
-    choices = [(c[0], int(c[1])) for c in choices]
-    start = masked_windows_df.xs("start", level=2)
-    end = masked_windows_df.xs("end", level=2)
-    low = start.loc[choices]
-    high = end.loc[choices]
-    t_start = rng.uniform(low=low.values, high=high.values)
-    lens_data = np.column_stack(([c0 for c0, c1 in choices],
-                                  t_start))
-    result = pd.DataFrame(data=lens_data, columns=["objectid", "t_start"])
-    result["copy"] = result.groupby(by="objectid").cumcount()
-    return result
+def parquet_to_mydb(read_dir, i_tau, version):
+    batches = np.arange(67)
+    schema = "objectid,text\nnumber,int\nstart,real\nend,real"
+    qc.mydb_drop(f"windows_temp")
+    qc.mydb_create(f"windows_temp", schema, drop=True)
 
-def make_temp_table(lens_df):
-    schema = "objectid,text\ncopy,int\nt_start,float8"
-    l = ["objectid", "copy", "t_start"]
-    qc.mydb_drop("temp")
-    qc.mydb_create("temp", schema, drop=True)
-    qc.mydb_insert("temp", lens_df[l].to_csv(index=False), schema=schema)
+    for batch in batches:
+        df_file = os.path.join(read_dir, 
+                               f"good_windows_batch{batch}_{version}.parquet")
 
-def make_lightcurve_dataframe():
-    sq = "SELECT * FROM mydb://temp"
-    q = """SELECT t.objectid, t.copy, t.t_start,
-               m.filter, m.mag_auto, m.magerr_auto, m.mjd, e.exptime
-           FROM nsc_dr2.meas AS m
-           INNER JOIN mydb://temp AS t
-           ON t.objectid = m.objectid
-           INNER JOIN nsc_dr2.exposure AS e
-           ON e.exposure = m.exposure
+        try:
+            window_df = pd.read_parquet(df_file, columns=[f"{i_tau}"]).dropna()
+        except ArrowInvalid as e:
+            continue
+
+        t_df = pd.concat([window_df.xs("start", level=2),
+                          window_df.xs("end", level=2)],
+                         axis=1, keys=["start", "end"])
+        t_df.reset_index(inplace=True)
+        t_df.columns = t_df.columns.get_level_values(0)
+        qc.mydb_insert(f"windows_temp", 
+                       t_df.to_csv(index=False), schema=schema)
+
+def t_start_table(n_windows, i_tau, version, drop=False):
+    id_q = "SELECT objectid FROM mydb://numbered_stable_stars_sep2"
+    sq = f"""
+    SELECT *
+        FROM mydb://windows_temp
+        WHERE objectid IN ({id_q})
+        ORDER BY RANDOM()
+        LIMIT {n_windows}"""
+    query = f"""
+    SELECT 
+        (ROW_NUMBER() OVER () - 1) AS rn,
+        w.objectid, 
+        w.number, 
+        w.start + (RANDOM() * (w.end - w.start)) AS t_start
+    FROM ({sq}) AS w"""
+    qc.query(sql=query, out=f"mydb://t_start_{i_tau}_{version}", drop=drop)
+    qc.mydb_index(f"t_start_{i_tau}_{version}", "objectid")
+    qc.mydb_index(f"t_start_{i_tau}_{version}", "rn")
+
+def make_lightcurve_dataframe(i_tau, i_batch, batch_size, version):
+    sq = f"""
+    SELECT *
+        FROM mydb://t_start_{i_tau}_{version}
+        WHERE rn BETWEEN {i_batch * batch_size} 
+        AND {(i_batch + 1) * batch_size - 1}
     """
-    result = qc.query(sql=q, fmt="pandas")
-    result["mjd_mid"] = result["mjd"] + (result["exptime"] / (86400 * 2))
-    result.sort_values(by=["objectid", "mjd"], inplace=True)
+    qc.query(sql=sq, out="mydb://temp", drop=True)
+    qc.mydb_index("temp", "objectid")
+    q = f"""
+    SELECT 
+        t.objectid, t.number, t.t_start, 
+        m.filter, m.mag_auto, m.magerr_auto, m.mjd,
+        m.exptime, (m.mjd + (m.exptime / (86400 * 2))) AS mjd_mid
+        FROM mydb://stable_lcs AS m
+        INNER JOIN mydb://temp AS t
+        ON t.objectid = m.objectid
+    """
+    job_id = qc.query(sql=q, async_=True, timeout=3600)
+
+    while qc.status(job_id) == "EXECUTING":
+        time.sleep(10)
+
+    if qc.status(job_id) == "COMPLETED":
+        result = convert(qc.results(job_id))
+        result.sort_values(by=["objectid", "mjd"], inplace=True)
+    elif qc.status(job_id) == "ERROR":
+        print(f"Error batch {i_batch}: {qc.error(job_id)}")
+        result = None
+    else:
+        print(f"Error batch {i_batch}: Something unexpected occurred")
+        result = None
+
+    qc.mydb_drop("temp")
     return result
 
 def lens_lc(lc, tau):
     t_start = lc["t_start"].iloc[0]
     idx0, idx1 = lc["mjd_mid"].searchsorted([t_start, t_start+tau])
-    vals = lc["mag_auto"].values
+    vals = lc["mag_auto"].to_numpy()
     vals[idx0: idx1] += -2.5 * np.log10(2)
     true_label = np.full(vals.shape, 1)
     true_label[idx0: idx1] = 0
@@ -64,21 +98,7 @@ def lens_lc(lc, tau):
     lc["true_label"] = true_label
     return lc
 
-def main(taus, tau_idx, unimodal_id, window_df, **kwargs):
-    tau_window = window_df[tau_idx].dropna()
-    lens_df = make_lens_df(unimodal_id, tau_window)
-    make_temp_table(lens_df)
-    lc = make_lightcurve_dataframe()
-    g = lc.groupby(by=["objectid", "copy", "filter"],
-                   sort=False,
-                   group_keys=False)
-    lc = g.filter(lambda x: len(x) > 3)
-    g = lc.groupby(by=["objectid", "copy"], sort=False, group_keys=False)
-    n_lensed = g.ngroups
-    lensed_df = g.apply(lens_lc, (taus[tau_idx]))
-    lensed_df = cluster_label_dataframe(lensed_df)
-    # g = lensed_df.groupby(by=["objectid", "copy"], sort=False, group_keys=False)
-    # lensed_df = g.filter(lambda group: lens_filter(group, **kwargs))
-    # label_df = lensed_df[["true_label", "cluster_label"]]
-    # result = label_df.set_index(["objectid", "copy", "t_start"])
-    return lensed_df
+def count_windows(df):
+    cl = df["cluster_label"].to_numpy()
+    result = len(get_bounding_idxs(cl))
+    return result
