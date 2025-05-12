f8d1181057dafab090fb32a194c59b4676682c07
adriansh95 <adrianshestakov@gmail.com>
2025-05-12 13:16:00 -0700

    updated import paths to reflect module renames

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,17 +1,17 @@
-from utils.tasks.kde_label_task import KDELabelTask
-from utils.tasks.summary_table_task import SummaryTableTask
-from utils.tasks.efficiency_task import EfficiencyTask
-from utils.tasks.analyze_backgrounds_task import AnalyzeBackgroundsTask
-from utils.tasks.bin_objects_task import BinObjectsTask
-from utils.tasks.event_rate_task import EventRateTask
-from utils.tasks.effective_monitoring_time_task import (
+from tasks.kde_label_task import KDELabelTask
+from tasks.summary_table_task import SummaryTableTask
+from tasks.efficiency_task import EfficiencyTask
+from tasks.analyze_backgrounds_task import AnalyzeBackgroundsTask
+from tasks.bin_objects_task import BinObjectsTask
+from tasks.event_rate_task import EventRateTask
+from tasks.effective_monitoring_time_task import (
     EffectiveMonitoringTimeTask
 )
-from utils.tasks.good_windows_task import GoodWindowsTask
-from utils.tasks.good_windows_by_duration_task import (
+from tasks.good_windows_task import GoodWindowsTask
+from tasks.good_windows_by_duration_task import (
     GoodWindowsByDurationTask
 )
-from utils.tasks.sample_good_windows_task import SampleGoodWindowsTask
+from tasks.sample_good_windows_task import SampleGoodWindowsTask
 
 __all__ = [
     "KDELabelTask",
