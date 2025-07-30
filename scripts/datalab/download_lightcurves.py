ad8039c33934953f5623827593fc973119c73063
adriansh95 <adrianshestakov@gmail.com>
2025-07-30 10:36:45 -0700

    download in batches of 50k and download more attributes along with lightcurves

--- scripts/datalab/download_lightcurves.py
+++ scripts/datalab/download_lightcurves.py
@@ -1,53 +1,70 @@
 import os
-import time
-import logging
-logger = logging.getLogger(__name__)
+import re
 
 from dl import queryClient as qc
-from dl.helpers.utils import convert
+#from dl.helpers.utils import convert
 from tqdm import tqdm
 
 def main():
-    log_dir = "/Users/adrianshestakov/Work/stringScratch/logs"
-    log_name = os.path.join(log_dir, "download_lightcurves.log")
-    logging.basicConfig(filename=log_name, level=logging.INFO)
-    n_batches = 67
-    batch_size = 100000
-    write_dir = "/Volumes/Thesis_data/"
-    qc.mydb_drop("temp_ids")
+    n_objects = 6.61e6
+    batch_size = 50000
+    n_batches = int((n_objects // batch_size) + 1)
+    write_dir = "/Volumes/BACKUP_2/lightcurves/"
 
-    for i_batch in tqdm(range(41, 42)):
+    for i_batch in tqdm(range(27, 132)):
+        print(i_batch)
         fname = f"lightcurves_batch{i_batch}.parquet"
-        sq = f"""SELECT id FROM mydb://ordered_lmc_ids
-                 WHERE row_number BETWEEN {i_batch * batch_size}
-                 AND {(i_batch + 1) * batch_size - 1}"""
-        qc.query(sql=sq, out="mydb://temp_ids")
-        qc.mydb_index("temp_ids", "id")
-        q = f"""SELECT m.objectid, m.filter, m.mag_auto, 
-                       m.magerr_auto, m.mjd, e.exptime,
-                       (m.mjd + (e.exptime / (2 * 86400))) AS mjd_mid
-                FROM nsc_dr2.meas AS m
-                INNER JOIN mydb://temp_ids AS t
-                ON t.id = m.objectid
-                INNER JOIN nsc_dr2.exposure AS e
-                ON e.exposure = m.exposure"""
+        id_query = f"""
+            SELECT 
+                id 
+            FROM 
+                mydb://ordered_lmc_ids
+            WHERE 
+                row_number BETWEEN {i_batch * batch_size}
+                AND {(i_batch + 1) * batch_size - 1}
+        """
 
-        job_id = qc.query(sql=q, timeout=2400, async_=True)
+        id_table_name = f"mydb://batch{i_batch}_ids"
+        qc.query(
+            sql=id_query,
+            timeout=600,
+            out=id_table_name,
+            drop=True
+        )
+        qc.mydb_index(id_table_name, "id")
 
-        while qc.status(job_id) == "EXECUTING":
-            time.sleep(10)
+        full_query = f"""
+            SELECT
+                M.asemi, M.bsemi, M.class_star, M.dec, M.exposure, E.exptime,
+                M.filter, M.flags, M.fwhm, M.kron_radius, M.mag_aper1,
+                M.mag_aper2, M.mag_aper4, M.mag_aper8, M.mag_auto,
+                M.magerr_aper1, M.magerr_aper2, M.magerr_aper4, M.magerr_aper8,
+                M.magerr_auto, M.mjd,
+                (M.mjd + (E.exptime / (2 * 86400))) AS mjd_mid, M.objectid,
+                M.ra, E.fwhm AS seeing, M.theta, M.x, M.y
+            FROM
+                nsc_dr2.meas AS M
+            INNER JOIN
+                {id_table_name} AS IDS
+            ON IDS.id = M.objectid
+            INNER JOIN
+                nsc_dr2.exposure AS E
+            ON E.exposure = M.exposure
+        """
 
-        if qc.status(job_id) == "COMPLETED":
-            df = convert(qc.results(job_id))
-            df.to_parquet(os.path.join(write_dir, fname))
-            logger.info(f"Wrote batch {i_batch}")
-        elif qc.status(job_id) == "ERROR":
-            e = qc.error(job_id)
-            logging.error(f"Error batch {i_batch}: {e}")
-        else:
-            s = f"Error batch {i_batch}: Something unexpected ocurred."
-            logging.error(s)
-        qc.mydb_drop("temp_ids")
+        df = qc.query(
+            sql=full_query,
+            timeout=86400,
+            async_=True,
+            wait=True,
+            verbose=True,
+            poll=10,
+            fmt="pandas"
+        )
+                
+        df.to_parquet(os.path.join(write_dir, fname))
+        qc.mydb_drop(id_table_name)
+        print(f"Wrote {os.path.join(write_dir, fname)}")
 
 if __name__ == "__main__":
     main()
