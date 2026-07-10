56e1a318619be868534846f5c0a8772aab34a1f4
adriansh95 <adrianshestakov@gmail.com>
2026-07-10 12:36:03 -0700

    modified to download lightcurves for demo.

--- scripts/datalab/download_lightcurves.py
+++ scripts/datalab/download_lightcurves.py
@@ -7,24 +7,37 @@ from tqdm import tqdm
 
 def main():
     n_objects = 6.61e6
-    batch_size = 50000
-    n_batches = int((n_objects // batch_size) + 1)
-    write_dir = "/Volumes/BACKUP_2/lightcurves/"
+    #batch_size = 50000
+    #n_batches = int((n_objects // batch_size) + 1)
+    n_batches = 1
+#    write_dir = (
+#        "/VOLUMES/THESIS1/lightcurves/"
+#    )
+    write_dir = "demo/"
 
-    for i_batch in tqdm(range(27, 132)):
+    for i_batch in tqdm(range(0, n_batches)):
         print(i_batch)
-        fname = f"lightcurves_batch{i_batch}.parquet"
+        fname = f"lightcurves.parquet"
+        #id_query = f"""
+        #    SELECT 
+        #        id 
+        #    FROM 
+        #        mydb://ordered_lmc_ids
+        #    WHERE 
+        #        row_number BETWEEN {i_batch * batch_size}
+        #        AND {(i_batch + 1) * batch_size - 1}
+        #"""
         id_query = f"""
             SELECT 
                 id 
             FROM 
                 mydb://ordered_lmc_ids
-            WHERE 
-                row_number BETWEEN {i_batch * batch_size}
-                AND {(i_batch + 1) * batch_size - 1}
+            ORDER BY RANDOM()
+            LIMIT 1000
         """
 
-        id_table_name = f"mydb://batch{i_batch}_ids"
+#        id_table_name = f"mydb://batch{i_batch}_ids"
+        id_table_name = "mydb://temp"
         qc.query(
             sql=id_query,
             timeout=600,
@@ -32,6 +45,17 @@ def main():
             drop=True
         )
         qc.mydb_index(id_table_name, "id")
+        mydb_query = f"SELECT id FROM {id_table_name}"
+        object_query = f"""
+            WITH IDS AS ({mydb_query})
+            SELECT 
+                O.id, O.ra, O.dec
+            FROM 
+                nsc_dr2.object AS O
+            INNER JOIN IDS ON O.id = IDS.id
+        """
+        objects = qc.query(sql=object_query, timeout=600, fmt="pandas")
+        objects.to_parquet("demo/objects/sampled_objects.parquet")
 
         full_query = f"""
             SELECT
@@ -45,26 +69,28 @@ def main():
             FROM
                 nsc_dr2.meas AS M
             INNER JOIN
-                {id_table_name} AS IDS
+                ({mydb_query}) AS IDS
             ON IDS.id = M.objectid
             INNER JOIN
                 nsc_dr2.exposure AS E
             ON E.exposure = M.exposure
         """
 
-        df = qc.query(
-            sql=full_query,
-            timeout=86400,
-            async_=True,
-            wait=True,
-            verbose=True,
-            poll=10,
-            fmt="pandas"
-        )
+#        df = qc.query(
+#            sql=full_query,
+#            async_=True,
+#            wait=True,
+#            poll=10,
+#            verbose=True,
+#            timeout=86400,
+#            fmt="pandas"
+#        )
+        df = qc.query(sql=full_query, timeout=600, fmt="pandas")
+        df.to_parquet("demo/lightcurves/all_lightcurves.parquet")
                 
-        df.to_parquet(os.path.join(write_dir, fname))
+        #df.to_parquet(os.path.join(write_dir, fname))
         qc.mydb_drop(id_table_name)
-        print(f"Wrote {os.path.join(write_dir, fname)}")
+        #print(f"Wrote {os.path.join(write_dir, fname)}")
 
 if __name__ == "__main__":
     main()
