a4d97931628bb8b26fddc3aec6f2d73a83fa35d1
adriansh95 <adrianshestakov@gmail.com>
2026-07-10 12:42:21 -0700

    added demo tasks, yaml, and requirements.txt

---
+++ demo/demo_tasks/__init__.py
@@ -0,0 +1,13 @@
+from tasks.kde_label_task import KDELabelTask
+from tasks.event_rate_task import EventRateTask
+from tasks.effective_monitoring_time_task import (
+    EffectiveMonitoringTimeTask
+)
+from tasks.good_windows_task import GoodWindowsTask
+
+__all__ = [
+    "KDELabelTask",
+    "EventRateTask",
+    "EffectiveMonitoringTimeTask",
+    "GoodWindowsTask"
+]
